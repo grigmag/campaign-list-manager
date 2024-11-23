e117dc82702b42795a1e46b4dd080a4b8abd4152
Greg Magkos <grmagkos@gmail.com>
2024-11-23 16:56:11 +0200

    Use the campaignMonitorService to get subscribers in our api route

--- src/app/api/subscribers/route.ts
+++ src/app/api/subscribers/route.ts
@@ -1,7 +1,19 @@
 import { NextResponse } from "next/server";
+import { campaignMonitorService } from "~/campaignMonitor/campaignMonitorService";
 
 export async function GET(_request: Request) {
-  return NextResponse.json({
-    data: [{ name: "John Doe", email: "john@doe.com" }],
-  });
+  try {
+    const results = await campaignMonitorService.getSubscribers();
+
+    return NextResponse.json({
+      data: results,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Failed to fetch subscribers" },
+      { status: 500 },
+    );
+  }
 }
